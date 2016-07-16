060780978287949c40a4aed2a39de4b7df8bba51
Icemic <bingfeng.jw@gmail.com>
2016-07-16 16:27:37 +0800

    fix: [Reactive] 解决各种渲染问题

--- src/Iceleaf.js
+++ src/Iceleaf.js
@@ -13,180 +13,178 @@ let Initialed = false;
 let Index = 0;
 
 function init() {
-	let viewNode = SpriteManager.init();
-	const TextWindowManager = TWMInit(SpriteManager);
-	document.body.appendChild(viewNode);
+    let viewNode = SpriteManager.init();
+    const TextWindowManager = TWMInit(SpriteManager);
+    document.body.appendChild(viewNode);
 
-	let stage = SpriteManager.getStage();
-	let renderer = SpriteManager.getRenderer();
+    let stage = SpriteManager.getStage();
+    let renderer = SpriteManager.getRenderer();
 
-	fps.installFPSView(stage);
-	update();
+    fps.installFPSView(stage);
+    update();
+    window.stage = stage;
 
-	function update(time) {
-		window.requestAnimationFrame(update.bind(this));
-		renderer.render(stage);
-		ActionManager.update(time);
-	}
+    function update(time) {
+        window.requestAnimationFrame(update.bind(this));
+        renderer.render(stage);
+        ActionManager.update(time);
+    }
 }
 
 export function render(el) {
-	if (!Initialed) {
-		init()
-	}
-	el.componentWillMount();
-	SpriteManager.getStage().addChild(el.getInstance());
-	el.componentDidMount();
+    if (!Initialed) {
+        init()
+    }
+    el.componentWillMount();
+    let instance = mountComponent(el);
+    SpriteManager.getStage().addChild(instance);
+    el.componentDidMount();
+}
+
+function mountComponent(el) {
+    let parent = el.getInstance();
+    bindEvents(el);
+    for (let childEl of el.children) {
+        let child = mountComponent(childEl);
+        parent.addChild(child);
+    }
+    return parent;
 }
 
 export function createElement(tag, params, ...childrenEl) {
-	let el;
-	params = params || {};
-	params.children = childrenEl;
-	if (typeof tag === 'string') {
-		switch (tag.toLowerCase()) {
-			// case 'layer': el = new LayerComponent(params);break;
-			case 'sprite': el = new SpriteComponent(params);break;
-			case 'text': el = new TextSpriteComponent(params);break;
-			default: Err.error(`[Iceleaf] Unknown element '${tag}'.`);break;
-		}
+    let el;
+    params = params || {};
+    params.children = childrenEl;
+    if (typeof tag === 'string') {
+        switch (tag.toLowerCase()) {
+            // case 'layer': el = new LayerComponent(params);break;
+            case 'sprite': el = new SpriteComponent(params);break;
+            case 'text': el = new TextSpriteComponent(params);break;
+            default: Err.error(`[Iceleaf] Unknown element '${tag}'.`);break;
+        }
 
-		el.name = tag.toLowerCase();
-	}
-	else {
-		el = new tag(params);
-	}
-	bindEvents(el, params);
+        el.name = tag.toLowerCase();
+    }
+    else {
+        el = new tag(params);
+    }
+    // 这时 params 应该已经挂载到 el.props
 
-	// 处理子对象
-	if (childrenEl) {
-		let parent = el.getInstance();
-		if (childrenEl instanceof Array) {
-			let childrenElExpanded = [];
-			for (let childEl of childrenEl) {
-				if (childEl instanceof Array) {
-					childrenElExpanded = childrenElExpanded.concat(childEl);
-				} else {
-					childrenElExpanded.push(childEl);
-				}
-			}
-			for (let childEl of childrenElExpanded) {
-				childEl.componentWillMount();
-				parent.addChild(childEl.getInstance());
-				childEl.componentDidMount();
-			}
-		} else {
-			let childEl = childrenEl;
-			childEl.componentWillMount();
-			parent.addChild(childEl.getInstance());
-			childEl.componentDidMount();
-		}
-	}
-	return el;
+    // 处理子对象
+    let childrenElExpanded = [];
+    if (childrenEl instanceof Array) {
+        for (let childEl of childrenEl) {
+            if (childEl instanceof Array) {
+                childrenElExpanded = childrenElExpanded.concat(childEl);
+            } else {
+                childrenElExpanded.push(childEl);
+            }
+        }
+    } else if (childrenEl){
+        childrenElExpanded = [childrenEl];
+    }
+
+    /*
+     * 区分原生组件和自定义组件。
+     * 原生组件的子组件（childrenEl）直接渲染为子节点（children）
+     * 自定义组件的子节点渲染由内部控制，子组件全部放置到 props
+     */
+    if (typeof tag === 'string') {
+        el.children = childrenElExpanded
+    } else {
+        el.children = [];
+        el.props.children = childrenElExpanded;
+    }
+
+    return el;
 }
 
-function bindEvents(el, params) {
-	params = params || {};
-	let obj = el.getInstance();
-	let keys = Object.keys(params);
-	for (let key of keys) {
-		if (/^on[A-Z]/.test(key)) {
-			obj[key.replace(/^on/, '').toLowerCase()] = params[key];
-		}
-	}
+function bindEvents(el) {
+    let obj = el.getInstance();
+    let keys = Object.keys(el.props);
+    for (let key of keys) {
+        if (/^on[A-Z]/.test(key)) {
+            obj[key.replace(/^on/, '').toLowerCase()] = el.props[key];
+        }
+    }
 }
 
 
 export class Component {
-	constructor(props) {
-		this.props = props || {};
-		this.state = {};
-	}
-	getInstance() {
-		return this.render().instance;
-	}
-	componentWillMount() {
+    constructor(props) {
+        this.props = props || {};
+        this.state = {};
 
-	}
-	componentDidMount() {
+        this.instance = null;
+    }
+    setState(state) {
+        Object.assign(this.state, state);
+    }
+    componentWillMount() {
 
-	}
-	componentWillUpdate() {
+    }
+    componentDidMount() {
 
-	}
-	componentDidUpdate() {
+    }
+    componentWillUpdate() {
 
-	}
-	render() {
-		return this;
-	}
+    }
+    componentDidUpdate() {
+
+    }
+    getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        else {
+            this.instance = mountComponent(this.render());
+            return this.instance;
+        }
+    }
+    render() {
+        return this;
+    }
+    update() {
+
+    }
 }
-// class LayerElement {
-// 	constructor({file, rect, x, y}) {
-// 		this.file = file;
-// 		this.rect = rect;
-// 		this.x = x || 0;
-// 		this.y = y || 0;
-//
-// 		this._instance = null;
-// 		this._index = null;
-// 	}
-// 	render() {
-// 		if (this._instance) {
-// 			return this._instance
-// 		} else {
-// 			SpriteManager.create(++Index, this.file, this.rect);
-// 			this._instance = SpriteManager.fromIndex(Index);
-// 			this._instance.x = this.x;
-// 			this._instance.y = this.y;
-// 			this._index = Index;
-// 			return this._instance;
-// 		}
-// 	}
-// }
-class SpriteComponent extends Component {
-	constructor({file, rect, x=0, y=0}) {
-		super({file, rect, x, y});
 
-		this.instance = null;
-		this.index = null;
-	}
-	getInstance() {
-		return this.render().instance;
-	}
-	render() {
-		if (this.instance) {
-			return this
-		} else {
-			SpriteManager.create(++Index, this.props.file, this.props.rect);
-			this.instance = SpriteManager.fromIndex(Index);
-			this.instance.x = this.props.x;
-			this.instance.y = this.props.y;
-			this.index = Index;
-			return this
-		}
-	}
+
+class SpriteComponent extends Component {
+    constructor({file, rect, x=0, y=0}) {
+        super({file, rect, x, y});
+
+        this.index = null;
+    }
+    render() {
+        if (this.instance) {
+            return this
+        } else {
+            SpriteManager.create(++Index, this.props.file, this.props.rect);
+            this.instance = SpriteManager.fromIndex(Index);
+            this.instance.x = this.props.x;
+            this.instance.y = this.props.y;
+            this.index = Index;
+            return this
+        }
+    }
 }
 class TextSpriteComponent extends Component {
-	constructor({text, x=0, y=0, ...options}) {
-		super({text, x, y, ...options});
+    constructor({text, x=0, y=0, ...options}) {
+        super({text, x, y, ...options});
 
-		this.instance = null;
-		this.index = null;
-	}
-	getInstance() {
-		return this.render().instance;
-	}
-	render() {
-		if (this.instance) {
-			return this;
-		} else {
-			SpriteManager.createText(++Index, this.props.text, this.props);
-			this.instance = SpriteManager.fromIndex(Index);
-			this.instance.x = this.props.x;
-			this.instance.y = this.props.y;
-			this.index = Index;
-			return this;
-		}
-	}
+        this.index = null;
+    }
+    render() {
+        if (this.instance) {
+            return this;
+        } else {
+            SpriteManager.createText(++Index, this.props.text, this.props);
+            this.instance = SpriteManager.fromIndex(Index);
+            this.instance.x = this.props.x;
+            this.instance.y = this.props.y;
+            this.index = Index;
+            return this;
+        }
+    }
 }
--- test/ui.jsx
+++ test/ui.jsx
@@ -3,30 +3,31 @@
 import {createElement, render, Component} from 'Iceleaf';
 
 class SpriteWithText extends Component {
-	constructor(props) {
-		super(props);
-	}
-	handleClick() {
-		console.log('文字框内文字被点击！');
-	}
-	render() {
-		return (
-			<Sprite file="assets/res/textwindow.png" x={this.props.x||0} y={this.props.y||0}>
-				<Text text='文字框内文字'
-					  color={0x000} size={20} x={50} y={50}
-		        	  font="思源黑体 Regular,思源黑体" onClick={this.handleClick}/>
-				{this.props.children}
-			</Sprite>
-		)
-	}
+    constructor(props) {
+        super(props);
+    }
+    handleClick() {
+        console.log('文字框内文字被点击！');
+    }
+    render() {
+        return (
+            <Sprite file="assets/res/textwindow.png" x={this.props.x||0} y={this.props.y||0}>
+                <Text text='文字框内文字'
+                      color={0x000} size={20} x={50} y={50}
+                      font="思源黑体 Regular,思源黑体" onClick={this.handleClick}>
+                </Text>
+                {this.props.children}
+            </Sprite>
+        )
+    }
 }
 
 module.exports = (
-	<Sprite file="assets/res/BG32a_1280.jpg">
-		<SpriteWithText x={100} y={450}>
-			<Sprite file="assets/res/ch-1.png" x={100} y={100} />
-			<Sprite file="assets/res/ch-2.png" x={130} y={100} />
-			<Sprite file="assets/res/ch-3.png" x={160} y={100} />
-		</SpriteWithText>
-	</Sprite>
+    <Sprite file="assets/res/BG32a_1280.jpg">
+        <SpriteWithText x={100} y={450}>
+            <Sprite file="assets/res/ch-1.png" x={100} y={100} />
+            <Sprite file="assets/res/ch-2.png" x={130} y={100} />
+            <Sprite file="assets/res/ch-3.png" x={160} y={100} />
+        </SpriteWithText>
+    </Sprite>
 )
