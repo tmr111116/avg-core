a4b9cf5d3910b483f23664ca10c7e4e074d67f68
Icemic <bingfeng.jw@gmail.com>
2016-09-12 15:58:18 +0800

    improve Textwindow component

--- src/core/Textwindow.js
+++ src/core/Textwindow.js
@@ -21,22 +21,37 @@ var RawTextwindow = createComponent('RawTextwindow', ContainerMixin, NodeMixin, 
     props.xInterval != null && layer.setXInterval(props.xInterval);
     props.yInterval != null && layer.setYInterval(props.yInterval);
     props.bgFile && layer.setBackgroundFile(props.bgFile);
-    props.opacity != null && layer.setBackgroundFile(props.opacity);
+    props.opacity != null && layer.setOpacity(props.opacity);
     props.visible != null && layer.setVisible(props.visible);
     props.font && layer.setTextFont(props.font);
     props.size && layer.setTextSize(props.size);
     props.color != null && layer.setTextColor(props.color);
     props.bold != null && layer.setTextBold(props.bold);
     props.italic != null && layer.setTextItalic(props.italic);
-    props.italic != null && layer.setTextItalic(props.italic);
     props.speed && layer.setTextSpeed(props.speed);
-    props.text && layer.drawText(props.text, true);
+    // props.text && layer.drawText(props.text, true);
     layer.x = props.x || 0;
     layer.y = props.y || 0;
     return layer;
   },
   updateNode(prevProps, props) {
     var layer = this.node;
+
+    prevProps.textRect !== props.textRect && layer.setTextRectangle(props.textRect);
+    prevProps.xInterval !== props.xInterval && layer.setXInterval(props.xInterval || 0);
+    prevProps.yInterval !== props.yInterval && layer.setYInterval(props.yInterval || 0);
+    prevProps.bgFile !== props.bgFile && layer.setBackgroundFile(props.bgFile);
+    prevProps.opacity !== props.opacity && layer.setOpacity(props.opacity);
+    prevProps.visible !== props.visible && layer.setVisible(props.visible);
+    prevProps.font !== props.font && layer.setTextFont(props.font);
+    prevProps.size !== props.size && layer.setTextSize(props.size);
+    prevProps.color !== props.color && layer.setTextColor(props.color);
+    prevProps.bold !== props.bold && layer.setTextBold(props.bold);
+    prevProps.italic !== props.italic && layer.setTextItalic(props.italic);
+    prevProps.speed !== props.speed && layer.setTextSpeed(props.speed);
+    layer.x = props.x || 0;
+    layer.y = props.y || 0;
+
 	//  layer.setProperties(props);
   }
 
@@ -51,32 +66,47 @@ export class Textwindow extends React.Component {
     // y: React.PropTypes.number
   };
   state = {
-
+    props: {}
   }
   execute(params, flags=[], name) {
     let layer = this.refs.layer;
-    let promise;
+    let promise = Promise.resolve;
+    let waitClick = false;
     if (flags.includes('clear')) {
-      promise = layer.clearText();
+      layer.clearText();
     }
-    if (flags.includes('continue')) {
+    if (flags.includes('set')) {
+      this.setState({
+        props: Object.assign({}, this.state.props, params)
+      });
+    } else if (flags.includes('continue')) {
+      waitClick = true;
       promise = layer.drawText(params.text, false);
     } else {
+      waitClick = true;
       promise = layer.drawText(params.text, true);
     }
     return {
-      waitClick: true,
+      waitClick: waitClick,
       promise: flags.includes('nowait') ? Promise.resolve() : promise,
       clickCallback: () => {
         layer.completeText();
       }
     };
   }
-  componentDidMount() {
-    // console.log(this)
+  getData() {
+
+  }
+  setData() {
+
+  }
+  componentWillMount() {
+    this.setState({
+      props: this.props
+    });
   }
   render() {
-    return <RawTextwindow {...this.props} ref={'layer'}>{this.props.children}</RawTextwindow>
+    return <RawTextwindow {...this.state.props} ref={'layer'}>{this.props.children}</RawTextwindow>
   }
 }
 
