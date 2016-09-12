4f9c3d91bdf1932d41f17a7e72244dc085f1e1df
Icemic <bingfeng.jw@gmail.com>
2016-09-12 15:59:10 +0800

    modify example

--- example/entry.js
+++ example/entry.js
@@ -1,9 +1,9 @@
 require("./assets/css/style.css");
 
-import { React, Component, render, Surface, Image, Text, Layer, Scene, Textwindow } from 'iceleaf';
+import { React, Component, render, Surface, Image, Text, Layer, Scene, Textwindow, BGImage, FGImage } from 'iceleaf';
 import DragableText from './DragableText';
 
-class Test extends Component {
+class Game extends Component {
   constructor(props) {
     super(props);
 
@@ -14,7 +14,7 @@ class Test extends Component {
   }
   componentDidMount() {
     let scene = this.refs.scene;
-    scene.bindCommand([this.refs.tw]);
+    scene.bindCommand([this.refs.tw, this.refs.fg, this.refs.bg]);
   }
   handleClick(e) {
     this.setState({
@@ -27,20 +27,18 @@ class Test extends Component {
     return (
       <Surface width={1280} height={720}>
         <Scene ref={'scene'} script={'script.bks'}>
-        <Image file="assets/res/BG32a_1280.jpg" ref='bg' width={640} height={480} color={0xff6600}>
-          <Textwindow bgFile="assets/res/textwindow.png"
-          color={0x0} visible={true} ref={'tw'} commandName={'text'}
+          <BGImage commandName='bg' ref='bg'/>
+          <Textwindow
+          color={0x0} visible={true} commandName={'text'} ref={'tw'}
           onClick={this.handleClick.bind(this)}>
             <DragableText text="测试文字 - 天依蓝" color={0x66ccff} x={100} y={100}/>
             {this.state.show ? <Image file="assets/res/ch-2.png" x={80} y={100}/> : null}
-            <Image file="assets/res/ch-1.png" x={50} y={100}/>
-            <Image file="assets/res/ch-3.png" x={110+this.state.x} y={100}/>
           </Textwindow>
-        </Image>
+          <FGImage commandName={'fg'} ref={'fg'} width={1280} height={720}/>
         </Scene>
       </Surface>
     )
   }
 }
 
-render(<Test />, document.getElementById('app'));
+render(<Game />, document.getElementById('app'));
