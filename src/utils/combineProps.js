8de72a055ad9e3d59c934c7792001e78c0338ff6
Icemic <bingfeng.jw@gmail.com>
2016-11-12 15:32:29 +0800

    add file

---
+++ src/utils/combineProps.js
@@ -0,0 +1,31 @@
+/**
+ * @file        Util to combine props between
+ * @author      Icemic Jia <bingfeng.web@gmail.com>
+ * @copyright   2015-2016 Icemic Jia
+ * @link        https://www.avgjs.org
+ * @license     Apache License 2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default function combineProps(props, filter) {
+  const validKeys = Object.keys(filter);
+  const keys = Object.keys(props);
+  const newProps = {};
+  for (let key of keys) {
+    if (validKeys.includes(key) && key !== 'children') {
+      newProps[key] = props[key];
+    }
+  }
+  return newProps;
+}
