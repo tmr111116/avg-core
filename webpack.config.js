4401370b6092753958b8bc15380bd9554554407d
Icemic <bingfeng.jw@gmail.com>
2017-01-22 00:32:09 +0800

    upgrade to webpack2, modify configs

--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,55 @@ const path = require('path');
 const webpack = require('webpack');
 const BitBarWebpackProgressPlugin = require('bitbar-webpack-progress-plugin');
 
-module.exports = {
-  entry: {
-    avg: ['babel-polyfill', 'whatwg-fetch', './src/avg.js'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    chunkFilename: '[name].min.js',
-    libraryTarget: 'umd',
-    library: 'AVG',
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-    modulesDirectories: ['src', 'node_modules'],
-  },
-  resolveLoader: {
-    fallback: [path.join(__dirname, 'node_modules')],
-  },
-  module: {
-    postLoaders: [
-      {
-        include: path.resolve(__dirname, 'node_modules', 'pixi.js'),
-        loader: 'transform/cacheable?brfs',
-      },
-    ],
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules\/(?!koa-compose)/, loader: 'babel-loader', query: { compact: true } },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader', query: { compact: true } },
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.(glsl|frag|vert)$/, loader: 'raw', exclude: /node_modules/ },
-      { test: /\.(glsl|frag|vert)$/, loader: 'glslify', exclude: /node_modules/ },
-      { test: path.resolve(__dirname, 'node_modules', 'pixi.js'), loader: 'ify' },
-    ],
-  },
-  externals: {  // 指定采用外部 CDN 依赖的资源，不被webpack打包
-    'pixi.js': 'PIXI',
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      VERSION: JSON.stringify(require("./package.json").version),
-      "process.env": { 
-        NODE_ENV: JSON.stringify("production"),
-      },
-    }),
-    new BitBarWebpackProgressPlugin(),
-  ],
-  devServer: {
-    contentBase: './example',
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-  },
+const packageInfo = require('./package.json');
+
+module.exports = function (env) {
+
+  let uglifyPlugin;
+
+  if (env.minimize) {
+    uglifyPlugin = [new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: true
+      }
+    })];
+  } else {
+    uglifyPlugin = [];
+  }
+
+  return {
+    entry: ['babel-polyfill', 'whatwg-fetch', './src/avg.js'],
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: `avg${env.minimize ? '.min' : ''}.js`,
+      libraryTarget: 'umd',
+      library: 'AVG',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      modules: ['src', 'node_modules'],
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules\/(?!koa-compose)/, loader: 'babel-loader', query: { compact: true } },
+        { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader', query: { compact: true } },
+        { test: /\.(glsl|frag|vert)$/, loader: 'raw-loader', exclude: /node_modules/ },
+      ],
+    },
+    externals: {
+      'pixi.js': 'PIXI',
+    },
+    devtool: 'source-map',
+    plugins: [
+      new webpack.DefinePlugin({
+        VERSION: JSON.stringify(packageInfo.version),
+        'process.env': {
+          NODE_ENV: JSON.stringify(env.release ? 'production' : 'development'),
+        },
+      }),
+      ...uglifyPlugin,
+      new BitBarWebpackProgressPlugin(),
+    ]
+  };
 };
